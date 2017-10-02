05f73ecd57ed10b983116250b3ce2982c7ffefce
Matthias L. Jugel <leo@thinkberg.com>
2017-10-02 11:34:09 +0200

    remove space

--- go_runtests.sh
+++ go_runtests.sh
@@ -4,4 +4,4 @@ mbed new .
 mbed target NRF52_DK
 mbed toolchain GCC_ARM
 mbed test --compile -n "$TESTS"
-mbedgt -n "$TESTS"  --plain --report-junit=testresult.xml --report-memory-metrics-csv=testmem.csv
+mbedgt -n "$TESTS" --plain --report-junit=testresult.xml --report-memory-metrics-csv=testmem.csv
